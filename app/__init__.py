22fd524a16f7ed320bd520149f84cb4ed2108f96
Leina33 <dominickrutto@gmail.com>
2019-12-05 15:08:26 +0300

    update the  __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from config import config_options
-from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,18 +11,27 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-
 def create_app(config_name):
 
-    app=Flask(__name__)
-    app.config.from_object(config_options[config_name])
-    bootstrap = Bootstrap(app)
-    login_manager.init_app(app)
-    #....
+    app = Flask(__name__)
     
-    # Registering the blueprint
+    #register blueprints
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
+    
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # Will add the views and forms
+
+# Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-    
     return app
