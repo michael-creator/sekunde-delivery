91dd7238fc87e7b12eb97b5f0ac2b2ce0864958e
Leina33 <dominickrutto@gmail.com>
2019-12-05 16:23:51 +0300

    set the migration file

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 from app.models import *
 
-# Set up migrations 
+# Set up migrations module
 from flask_migrate import Migrate,MigrateCommand
 
 # Creating app instance
