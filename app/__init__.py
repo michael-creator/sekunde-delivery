9511c545f9f3ece089c4f40e898cd4910cabb196
Dancanoduor <dancanoduor1990@gmail>
2019-12-05 12:59:39 +0300

    add function that display username

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from config import config_options
-
+from flask_bootstrap import Bootstrap
 
 
 def create_app(config_name):
 
     app=Flask(__name__)
     app.config.from_object(config_options[config_name])
+
+    bootstrap = Bootstrap(app)
     #....
     # Registering the blueprint
     from .main import main as main_blueprint
