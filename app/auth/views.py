73226ff193c1ddcee70a010d52466d864e42d8bf
Leina33 <dominickrutto@gmail.com>
2019-12-05 16:09:50 +0300

    created class Role

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,7 @@
-from flask import from flask import render_template,redirect,url_for, flash,request
+from flask import render_template,redirect,url_for, flash,request
 from flask_login import login_user,logout_user,login_required
 from ..models import User
+from . import auth
 from .forms import RegistrationForm, LoginForm
 
 
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-rom . import db
+from . import db
 from sqlalchemy.sql import func
 from . import login_manager
 from flask_login import UserMixin, current_user
@@ -36,5 +36,12 @@ class User(UserMixin,db.Model):
     def __repr__(self):
         return f'{self.username}'
 
-
+class Role(db.Model):
+    __tablename__ = 'roles'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    
+    def __repr__(self):
+        return f'User {self.name}'
 
