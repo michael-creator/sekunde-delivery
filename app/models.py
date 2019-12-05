a484c776970b74da53e17ac35cf69040d26a1f25
Leina33 <dominickrutto@gmail.com>
2019-12-05 15:28:20 +0300

    this is the class user with attributes id,username and email

--- app/models.py
+++ app/models.py
@@ -1,17 +1,40 @@
-from . import db
-
-from flask_login import UserMixin
+rom . import db
+from sqlalchemy.sql import func
 from . import login_manager
+from flask_login import UserMixin, current_user
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(UserMixin,db.Model):
-    __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255),index = True)
+
+class User(UserMixin,db.Model):
+    
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'{self.username}'
+
+
+
